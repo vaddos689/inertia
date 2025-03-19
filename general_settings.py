b6256c6b968e9312732a94d852611a9542b203af
vaddos689 <roganfanboy@gmail.com>
2025-03-19 17:04:43 +0300

    upd

--- general_settings.py
+++ general_settings.py
@@ -2,7 +2,7 @@ import asyncio
 
 CAPSOLVER_API_KEY = ''
 
-ACCOUNTS_RANGE = [21, 21]  # from 1 to ... (if 0, 0 then all accounts)
+ACCOUNTS_RANGE = [0, 0]  # from 1 to ... (if 0, 0 then all accounts)
 
 RANDOM_PAUSE_BETWEEN_ACCOUNTS = [5, 15]
 
